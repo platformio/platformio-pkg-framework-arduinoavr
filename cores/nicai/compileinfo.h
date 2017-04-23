53995f3718536b00a8d42168110b516f7eb3623f
Ivan Kravets <me@ikravets.com>
2017-04-23 23:41:44 +0300

    Set version for robolib

--- cores/nicai/compileinfo.h
+++ cores/nicai/compileinfo.h
@@ -1,5 +1,6 @@
 #include <avr/io.h>
-#include "version.h"
+// #include "version.h"
+#define VERSION "3.6.0"
 #define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
 #define COMPILEINFO "<compileinfo avr-gcc='" __VERSION__ "' avrlibc='" __AVR_LIBC_VERSION_STRING__ "' robolib='" VERSION "' />"
 #if GCC_VERSION > 40400
